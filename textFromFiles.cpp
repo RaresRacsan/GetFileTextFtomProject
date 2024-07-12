ab49b10b3405edb9dfe3c595f86e21bb35c8ea2c
916-Racsan-Rares <rares.racsan@gmail.com>
2024-07-12 15:32:18 +0300

    final code with comments

--- textFromFiles.cpp
+++ textFromFiles.cpp
@@ -6,28 +6,38 @@
 namespace fs = std::filesystem;
 
 int main() {
-    // change the name of the merging file
+    // change the name of the result file
     std::string outputFileName = "merged_files.txt";
+    // creating an output file
     std::ofstream outputFile(outputFileName);
 
+    // failed to create
     if (!outputFile.is_open()) {
         std::cerr << "Failed to create the output file." << std::endl;
         return 1;
     }
 
-    for (const auto& entry : fs::directory_iterator(fs::current_path())) {
+    // this file's name
+    std::string currentSourceFile = "textFromFiles.cpp";
+
+    // parsing the files from the current directory
+    for (const auto& entry : fs::directory_iterator(fs::current_path())) {\
+        // is a normal file
         if (entry.is_regular_file()) {
             std::string filePath = entry.path().string();
+            std::string fileName = entry.path().filename().string();
             std::string extension = entry.path().extension().string();
             // change the extensions for what you need
-            if (extension == ".cpp" || extension == ".h") {
+            if (fileName != currentSourceFile && (extension == ".cpp" || extension == ".h")) {
                 std::ifstream inputFile(filePath);
 
+                // can't open the file in order to get the content
                 if (!inputFile.is_open()) {
                     std::cerr << "Failed to open file: " << filePath << std::endl;
                     continue;
                 }
 
+                // printing the content of a file into the final file
                 std::string fileName = entry.path().filename().string();
                 outputFile << "File: " << fileName << "\n";
                 outputFile << std::string((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
@@ -39,6 +49,7 @@ int main() {
     }
 
     outputFile.close();
+    // success
     std::cout << "All files have been merged into " << outputFileName << std::endl;
 
     return 0;
